1db75f32697f3449050c8eb17fce566aaccb546b
Viktor Pankratov <pankratov@uchi.ru>
2016-11-18 11:32:21 +0300

    Reset migrations

--- db/migrate/20161116091500_add_user_name_to_answers.rb
+++
@@ -1,7 +0,0 @@
-class AddUserNameToAnswers < ActiveRecord::Migration
-  def change
-  	change_table :answers do |t|
-  		t.string :user_name
-  	end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116091500) do
+ActiveRecord::Schema.define(version: 20161111100540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string   "content"
+    t.string   "user_name"
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "question_id"
-    t.string   "user_name"
   end
 
   create_table "questions", force: :cascade do |t|
     t.string   "content"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "label"
   end
+
+  add_index "questions", ["user_id", "created_at"], name: "index_questions_on_user_id_and_created_at", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
     t.string   "role"
