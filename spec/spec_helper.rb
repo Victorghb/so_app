c005c587137278eadd6871ac72b3439a88971a10
Viktor Pankratov <pankratov@uchi.ru>
2016-11-22 13:18:34 +0300

    fix tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-ActiveRecord::Migration.maintain_test_schema!
+#ActiveRecord::Migration.maintain_test_schema!
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
